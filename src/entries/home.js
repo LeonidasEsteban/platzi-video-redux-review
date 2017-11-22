c8f4c2e4fdc934bc9bea52d74f38242ae078b94a
Leonidas Esteban <leonidasesteban@gmail.com>
2017-11-22 03:16:08 -0500

    adding normalization

--- src/entries/home.js
+++ src/entries/home.js
@@ -8,10 +8,35 @@ import { Provider } from 'react-redux';
 
 import reducer from '../reducers/search';
 
+import { normalize, schema } from 'normalizr';
+
+
+
+// const xyw = new schema.Entity('category', options = {idAttribute:})
+
+
+const media = new schema.Entity('media', {}, {
+  idAttribute: 'id',
+  processStrategy: (value, parent, key) => ({...value, category: parent.id})
+});
+
+const category = new schema.Entity('categories', {
+  playlist: new schema.Array(media),
+});
+
+// Entity(key, definition = {}, options = {})
+
+const categories = { categories: new schema.Array(category) }
+
+// const normalizedData = normalize(data, schema);
+const normalizedData = normalize(api, categories);
+console.log(normalizedData);
+
 const initialState = {
   ...api,
   query: '',
-  searchResults: []
+  searchResults: [],
+  entities: normalizedData.entities
 }
 
 const store = createStore(
