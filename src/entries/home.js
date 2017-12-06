58f4e14d6924891fabba341f72fae724c097daf1
Leonidas Esteban <leonidasesteban@gmail.com>
2017-12-06 02:37:41 -0500

    applyMiddlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Home from '../pages/home';
 import api from '../api.json';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 // import reducer from '../reducers/search';
@@ -29,11 +29,29 @@ const initialState = fromJS({
 })
 
 
+// function logger({ getState, dispatch }) {
+//   return (next) => {
+//     return (action) => {
+//       console.log('Vamos a enviar está acción', action)
+//       let returnValue = next(action)
+//       console.log('Este es el nuevo estado de la aplicación', getState().toJS())
+//       return returnValue
+//     }
+//   }
+// }
+
+const logger = ({ getState, dispatch }) => next => action => {
+  console.log('Vamos a enviar está acción', action)
+  let returnValue = next(action)
+  console.log('Este es el nuevo estado de la aplicación', getState().toJS())
+  return returnValue
+}
 
 const store = createStore(
   reducer,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger)
 );
 
 ReactDOM.render(
